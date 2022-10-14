d759c2e4a07337c5333081aebcd34616c6b697fd
강소연 <nebula059@naver.com>
2022-10-14 22:52:08 +0900

    Like Entity 연습 작성

--- src/main/java/com/sparta/project/entity/Like.java
+++ src/main/java/com/sparta/project/entity/Like.java
@@ -1,4 +1,10 @@
 package com.sparta.project.entity;
 
 public class Like {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int result = a + b;
+        System.out.println(result);
+    }
 }
